78eb18ac3d649a32db02f6b207b3e7d8996c82d8
Alber Ashraf <beroooberooo6@gmail.com>
2023-04-23 03:43:37 +0200

    adding about us view

--- encryption/Controllers/HomeController.cs
+++ encryption/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace encryption.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
